3603db0cbcc85faee92a5b8261ec67101a49185d
NaN-Hempel <nanne.hempel@gmail.com>
2024-01-25 14:02:14 +0100

    Intermediate push. my_experiment+randomise

---
+++ code_files/visualisation/my_experiment.py
@@ -0,0 +1,42 @@
+"""
+Experiment class for generating data.
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+
+class Experiment:
+    def __init__(self, exp_args, board_args):
+        self.exp_args = exp_args
+        self.board_args = board_args
+        self.data = pd.DataFrame()
+        pass
+
+    def a_star(self):
+        pass
+
+    def breadth_first(self):
+        pass
+
+    def depth_first(self):
+        pass
+
+    def randomise(self):
+        pass
+
+    def histogram(self):
+        pass
+
+    def plot(self):
+        pass
+
+    def save(self):
+        pass
+
+    def run(self):
+        pass
+
