378aeaeb0e24a47f71716d15dc1677ea6683df7a
jfosseweijer <jfosseweijer@gmail.com>
2024-01-30 17:01:22 +0100

    broad search resetten

--- code_files/algorithms/breadth_search.py
+++ code_files/algorithms/breadth_search.py
@@ -24,52 +24,37 @@ def breadth_search(current_layer_boards: list, current_layer_index, former_layer
         current_layer_index = None
         current_layer_boards.clear()
 
-    print(f"Lenght{len(former_layer_boards)}")
-    print(f"Index {former_layer_index}")
+
     board = former_layer_boards[former_layer_index][0]
 
     options = []
-    new_move = False
     # For each vehicle on the board, check if it can move
-    while new_move == False:
-        for vehicle in board.vehicles_list:
+    for vehicle in board.vehicles_list:
 
-            for direction in [-1, 1]:
-                move = 1
-                stuck = False
+        for direction in [-1, 1]:
+            move = 1
+            stuck = False
+            
+            while move < board.size and not stuck:
+                try:
+                    if not (vehicle.name == former_layer_boards[former_layer_index][1][0] and (move*direction) == -former_layer_boards[former_layer_index][1][1]):
+                        new_positions = board.move_piece(vehicle.name, move * direction)
+                        options.append((vehicle.name, (move * direction, new_positions)))
+                    move += 1
+
+                except ValueError:
+                    stuck = True
                 
-                while move < board.size and not stuck:
-                    try:
-                        if not (vehicle.name == former_layer_boards[former_layer_index][1][0] and (move*direction) == -former_layer_boards[former_layer_index][1][1]):
-                            new_positions = board.move_piece(vehicle.name, move * direction)
-                            options.append((vehicle.name, (move * direction, new_positions)))
-                        move += 1
+    if len(options) == 0:
+        raise ValueError("No possible moves found")
+    
+    # Select a random vehicle and a random move
+    if current_layer_index == None:
+        current_layer_index = len(options)
 
-                    except ValueError:
-                        stuck = True
-                    
-        if len(options) == 0:
-            raise ValueError("No possible moves found")
-        
-        # Select a random vehicle and a random move
-        if current_layer_index == None:
-            current_layer_index = len(options)
+    if current_layer_index >= 0:
+        current_layer_index -= 1
+        vehicle = options[current_layer_index][0]
+        movement, position = options[current_layer_index][1]
 
-        if current_layer_index >= 0:
-            new_move = True
-            current_layer_index -= 1
-            name = options[current_layer_index][0]
-            movement, position = options[current_layer_index][1]
-
-            vehicle = board.find_vehicle(name)
-            board.update_positions_set(vehicle, position)
-
-            for former_boards in former_layer_boards:
-                print (former_boards[0].vehicle_position_set)
-                if len(board.vehicle_position_set - former_boards[0].vehicle_position_set) == 0:
-                    new_move = False
-
-        print(new_move)
-
-
-    return name, movement, position, current_layer_boards, current_layer_index, former_layer_boards, former_layer_index
+    return vehicle, movement, position, current_layer_boards, current_layer_index, former_layer_boards, former_layer_index
